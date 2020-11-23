7298212e2577ed7e28f604000c74d5d6ae12e00f
dan <danperry1808@gmail.com>
2020-11-23 16:41:43 +0000

    Fix to user login and upload test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe 'Adding users' do
-  specify 'I can create users from a csv file, assign them a team, then login as a created user' do
+  specify 'I can create users from a tsv file, assign them a team, then login as a created user' do
     # Create staff user and login
     staff = create(:user, staff: true)
     login_as staff, scope: :user
     # Upload a csv of user information
     visit '/upload/users'
-    attach_file 'spec/uploads/users.csv'
+    attach_file 'spec/uploads/users.tsv'
     click_button 'Import'
     # Create module
     mod = create(:uni_module)
@@ -20,8 +20,10 @@ describe 'Adding users' do
     u1 = User.find_by_username('zzz12tu')
     login_as u1, scope: :user
     visit home_student_home_url
+    # Check that the user can only see their own team on the dashboard
     within(:css, '#teams'){
       expect(page).to have_content mod.name + " - Team 1"
+      expect(page).to_not have_content mod.name + " - Team 2"
     }
     # Repeat with team 2
     u2 = User.find_by_username('zzv12tu')
@@ -29,6 +31,7 @@ describe 'Adding users' do
     visit home_student_home_url
     within(:css, '#teams'){
       expect(page).to have_content mod.name + " - Team 2"
+      expect(page).to_not have_content mod.name + " - Team 1"
     }
   end
 
