545e07e229e2d98aad9cf288c37ec0289a3148d0
thomasdamri <tdamri1@sheffield.ac.uk>
2022-02-03 19:06:01 +0000

    Added student tasks table

---
+++ db/migrate/20220203183058_add_student_tasks_table.rb
@@ -0,0 +1,20 @@
+class AddStudentTasksTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :student_tasks do |t|
+      t.bigint :student_team_id
+      
+
+      t.string :task_title, null: false
+      t.string :task_objective
+
+      t.integer :task_difficulty, default: 0, null: false
+      t.integer :task_progress, default: 0, null: false
+
+      t.date :task_start_date, default: { 'CURRENT_DATE' }
+      t.date :task_target_date, null: false
+      t.date :task_complete_date
+
+    end
+    add_foreign_key :student_tasks, :student_teams, column: :student_team_id, on_delete: :cascade
+  end
+end
